cecc33dd8b7f5cb9047c885fe2edc2409dd976f1
Victor Volkov <vityan2504@gmail.com>
2022-01-31 13:58:57 +0300

    add (script.js) addes second task

--- script.js
+++ script.js
@@ -19,9 +19,17 @@ switch(lang) {
       break;
    case 'en' :
       console.log('Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
+      break;
 }
 
-lang === 'ru' ? console.log( arr[0].toString() ) : console.log( arr[1].toString() );
+lang === 'ru' ? console.log( arr[0].toString() ) : 
+lang === 'ru'? console.log( arr[1].toString() ) : console.log('Нет такого языка');
 
 
 ///////////////////
+
+const namePerson = prompt('Введите имя').toLowerCase();
+
+namePerson === 'артем' ? console.log(namePerson[0].toUpperCase() + namePerson.slice(1)  + ' директор') :
+namePerson === 'александр' ? console.log(namePerson[0].toUpperCase() + namePerson.slice(1) + ' преподаватель') : 
+console.log(namePerson[0].toUpperCase() + namePerson.slice(1) + ' студент');
