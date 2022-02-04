717da959ff08bb9b7ad3e9af69afab89e9349e32
Victor Volkov <vityan2504@gmail.com>
2022-02-04 13:40:47 +0300

    add (script.js) did task

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const date = new Date();
+
+const week = [
+   'Воскресенье',
+   'Понедельник',
+   'Вторник',
+   'Среда',
+   'Четверг',
+   'Пятница',
+   'Суббота'
+];
+
+week.forEach(function(item, index) {
+   if(index === date.getDay()) {
+      document.body.innerHTML += `<b>${item}</b><br>`;
+      return;
+   }
+   if(index === 0 || index === 6) {
+      document.body.innerHTML += `<i>${item}</i><br>`;
+      return;
+   }
+
+   document.body.innerHTML += `${item}<br>`;
+});
